6b4207e6639bcd176ef0e5592b75272bbc3c7c92
Khanzaadeh <khanzadeh008@gmail.com>
2022-10-04 22:51:34 +0600

    Snake game setup added.

---
+++ Projects/Snake Game/main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+import random
+import settings
+import Snake
+import Food
+
+def next_turn():
+    pass
+
+def change_direction():
+    pass
+
+def check_collision():
+    pass
+
+def game_over():
+    pass
+
